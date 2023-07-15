39eb2816fe022bc072c8dd1f4eafdfe60ba08394
eslamelshenawy <eslamelshenawy9316@gmail.com>
2023-07-15 16:25:01 +0300

    add second ubuntu comment

--- gateway-service/src/main/java/vmware/services/gateway/config/SecurityConfig.java
+++ gateway-service/src/main/java/vmware/services/gateway/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .exceptionHandling().authenticationEntryPoint(invalidLoginAttemptHandler)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/authenticate/**")
+                .antMatchers("/","/authenticate/**")
                 .permitAll()
                 .anyRequest().authenticated();
          http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);

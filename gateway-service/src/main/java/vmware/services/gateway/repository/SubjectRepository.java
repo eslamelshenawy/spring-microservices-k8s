815ed7913a22fbc95203a307cf5e1974b1dacde0
eslamelshenawy <eslamelshenawy9316@gmail.com>
2023-07-17 15:46:50 +0300

    add second ubuntu comment

--- gateway-service/src/main/java/vmware/services/gateway/repository/SubjectRepository.java
+++
@@ -1,8 +0,0 @@
-package vmware.services.gateway.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import vmware.services.gateway.entity.Subject;
-@Repository
-public interface SubjectRepository  extends MongoRepository<Subject,Long> {
-}
--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -15,16 +15,20 @@ eval $(minikube docker-env)
 docker images
 
 mvn clean
-
+mvn package
 
 cd gateway-service
 docker build -t vmware/gateway:1.1 .
 cd ..
 
 cd organization-service
+mvn clean
+mvn package
 docker build -t vmware/organization:1.1 .
 cd ..
 cd user-service
+mvn clean
+mvn package
 docker build -t vmware/user:1.1 .
 cd ..
 
